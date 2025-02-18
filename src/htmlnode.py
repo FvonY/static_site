c3f90aa60af1f0cc4efe47bb703f8b713d91092a
Johannes Schulze <schulze.joh@gmail.com>
2025-02-18 20:24:31 +0100

    CH2 - L3

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+        
+    def to_html(self):
+        raise NotImplementedError()
+    
+    def props_to_html(self):
+        html_props = ""
+        for prop in self.props.items():
+            key, value = prop
+            html_props += f' {key}="{value}"'
+        return html_props
+
+    def __repr__(self):
+        return (f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})")
+        
